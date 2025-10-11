b7b48ae5df7eb041e5d516caf8a24a04e80dcc57
jbrosan <john.brosan@gmail.com>
2025-10-10 22:59:16 -0400

    Initial Commit

---
+++ app/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <UApp>
+    <NuxtRouteAnnouncer />
+    <div class="min-h-screen flex flex-col">
+      <Header />
+      <main class="flex-grow p-4 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+        <h1 class="text-3xl font-bold italic">
+          Hello world!
+        </h1>
+        <!-- <NuxtPage /> -->
+      </main>
+      <Footer />
+    </div>
+  </UApp>
+</template>
